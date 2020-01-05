50f6dc17b1ab7f43d915f49e08da6492b144de8e
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-05 13:11:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -64,7 +64,6 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-  programmer_hash
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -80,7 +79,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+  programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
     
 end
 
